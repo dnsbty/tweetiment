71c4e4475ff59066fda8c47a2bf3703d72557bee
Dennis Beatty <dennis@dcbeatty.com>
2015-05-12 16:34:29 -0600

    Basic sentiment coloring

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -6,18 +6,25 @@ $(function(){
     var terms = encodeURIComponent($('#terms').val());
     console.log('Terms: ' + terms);
 
+    //send the request to the server
   	$.getJSON( '/search/' + terms, function( data ) {
       var count = data.statuses.length,
           total = 0;
-      $('.tweets').html('<table id="tweets"></table>');
       $.each(data.statuses, function( index, tweet ){
         console.log(tweet);
-        $('.tweets').prepend('<div class="tweet" id="'+tweet.id+'"></div>')
+        $('.tweets').append('<div class="tweet" id="'+tweet.id+'"></div>')
                     .attr('data-sentiment', tweet.sentiment.score);
         $('#'+tweet.id).append('<img class="profile-image" src="'+tweet.user.profile_image_url+'">')
                        .append('<h2>'+tweet.user.screen_name+'</h2>')
                        .append('<p class="tweet">'+tweet.text+'</p>');
         $('#'+tweet.id+' p.tweet').tweetParser();
+
+        if (tweet.sentiment.score > 0)
+          $('#'+tweet.id).css('background-color', 'rgba(214, 233, 198, '+tweet.sentiment.score/10+')')
+                         .css('color', '#3c763d');
+        else if (tweet.sentiment.score < 0)
+          $('#'+tweet.id).css('background-color', 'rgba(221, 153, 153, '+Math.abs(tweet.sentiment.score)/10+')')
+                         .css('color', '#a94442');
 
         total += tweet.sentiment.score;
       });
